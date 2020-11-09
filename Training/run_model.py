9d5a8215dd8df229a6ecd0c275c0025c5d68bc79
Ab Qyum <qyumjahid@gmail.com>
2020-11-09 13:05:29 +0600

    Update run_model.py

--- Training/run_model.py
+++ Training/run_model.py
@@ -12,3 +12,18 @@ history_1= model_1.fit(
     #callbacks=callbacks
     #use_multiprocessing=True,
 )
+
+# evaluate the model
+_, train_acc = model_1.evaluate(train_generator, verbose=0)
+_, test_acc = model_1.evaluate(validation_generator, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+
+#test image.......................
+
+import cv2
+img = cv2.imread('/content/drive/My Drive/deepfake_data/Capture_3.JPG')
+img = cv2.resize(img,(380,380))
+img = np.reshape(img,[1,380,380,3])
+
+classes =model_1.predict_classes(img)
+print(classes)
