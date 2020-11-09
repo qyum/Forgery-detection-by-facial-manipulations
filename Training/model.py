8160445fa246a6806c3e2e5115a9274bcfbf9eff
Ab Qyum <qyumjahid@gmail.com>
2020-11-09 12:58:35 +0600

    Create model.py

---
+++ Training/model.py
@@ -0,0 +1,21 @@
+
+LABEL_SMOOTHING = 0.05
+def build_model_1(IMAGE_WIDTH,IMAGE_HEIGHT,IMAGE_CHANNELS):
+    model = tf.keras.Sequential([
+        efn.EfficientNetB7(input_shape=(IMAGE_WIDTH,IMAGE_HEIGHT,IMAGE_CHANNELS),weights='imagenet',pooling='avg',include_top=False),
+        Dense(2, activation='softmax')
+        ])
+    
+    model.compile(optimizer='adam',
+                  loss = tf.keras.losses.CategoricalCrossentropy(label_smoothing = LABEL_SMOOTHING),
+                  #loss='categorical_crossentropy',
+                  metrics=[tf.keras.metrics.AUC(name='auc')])
+     
+    #model.add(Dense(2, activation='softmax'))  
+
+   # model.compile(loss='categorical_crossentropy',
+                  #optimizer='rmsprop',
+                  #metrics=[tf.keras.metrics.AUC(name='auc')])
+    return model 
+model_1=build_model_1(IMAGE_WIDTH, IMAGE_HEIGHT,IMAGE_CHANNELS)
+model_1.summary()
